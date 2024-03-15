8e0c6cd7843d75bb541e597e13a119f5c6692cab
nhussaini <nasrullah_hussaini@yahoo.com>
2024-03-14 22:59:31 -0400

    send list of connected users upon connecting to server

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,27 @@ expressWs(app);
 //   res.status(200).send('Welcome to SkillReactor');
 // });
 
+//store connected users
+let connectedUsers: String[] = [];
+
 (app as any).ws('/', (ws: any, req: any) => {
   const username: string = req.query.username;
   console.log('username->', username);
   // Log successful connection
   console.log(`WebSocket connection established for user: ${username}`);
+  connectedUsers.push(username);
+  console.log('connected users==>', connectedUsers);
+  sendUserList(ws);
 });
+
+// Function to send the user list message to a specific client
+function sendUserList(client: WebSocket) {
+  const userListMessage = JSON.stringify({
+    type: 'users',
+    data: connectedUsers,
+  });
+  client.send(userListMessage);
+}
 
 export default app;
 
